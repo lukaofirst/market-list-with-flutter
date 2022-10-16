364d57dc7a82a00ecb490075fc88652636a9e734
lukaofirst <lucasmacedo123@gmail.com>
2022-10-16 20:45:33 -0300

    first commit and setting up route's screens

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:market_list_flutter/screen/check_list_screen.dart';
+import 'package:market_list_flutter/screen/save_list_screen.dart';
+
+import 'screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Compras',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        primaryColor: Colors.white,
+      ),
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        SaveListScreen.routeName: (context) => SaveListScreen(),
+        CheckListScreen.routeName: (context) => CheckListScreen()
+      },
+    );
+  }
+}
