c007df2bed0b3fd6984fc1829007c960659d0ea8
lukaofirst <lucasmacedo123@gmail.com>
2024-12-25 21:53:18 -0300

    registering use cases and repositories

---
+++ market_list_with_flutter/lib/ioc/ioc.dart
@@ -0,0 +1,37 @@
+import 'package:get_it/get_it.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/upsert_product_use_case_impl.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/delete_product_use_case_impl.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/get_all_products_use_case_impl.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/delete_all_products_use_case_impl.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/interfaces/upsert_product_use_case.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/interfaces/delete_product_use_case.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/interfaces/get_all_product_use_case.dart';
+import 'package:market_list_with_flutter/application/use_cases/products/interfaces/delete_all_products_use_case.dart';
+import 'package:market_list_with_flutter/infra/persistence/interfaces/product_repository.dart';
+import 'package:market_list_with_flutter/infra/persistence/shared_preferences/product_repository_impl.dart';
+
+final getIt = GetIt.instance;
+
+class IoC {
+  IoC addRepositories() {
+    getIt.registerSingleton<ProductRepository>(ProductRepositoryImpl());
+
+    return this;
+  }
+
+  IoC addUseCases() {
+    getIt.registerSingleton<GetAllProductUseCase>(
+        GetAllProductsUseCaseImpl(getIt.get<ProductRepository>()));
+
+    getIt.registerSingleton<UpsertProductUseCase>(
+        UpsertProductUseCaseImpl(getIt.get<ProductRepository>()));
+
+    getIt.registerSingleton<DeleteProductUseCase>(
+        DeleteProductUseCaseImpl(getIt.get<ProductRepository>()));
+
+    getIt.registerSingleton<DeleteAllProductsUseCase>(
+        DeleteAllProductsUseCaseImpl(getIt.get<ProductRepository>()));
+
+    return this;
+  }
+}
